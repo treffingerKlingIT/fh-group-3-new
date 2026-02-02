9835796066587c9e76dd3e3e6a3c5a2caa9ee730
vickybremer <58940112+vickybremer@users.noreply.github.com>
2026-02-02 15:53:09 +0100

    new-event-popup and members styling

--- src/app/pages/tab3/run/run.page.ts
+++ src/app/pages/tab3/run/run.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-run',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./run.page.scss'],
 })
 export class RunPage implements OnInit {
+  // This allows us to control the modal in the HTML
+  @ViewChild(IonModal) modal!: IonModal;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Function to close the popup when 'Cancel' or 'Publish' is clicked
+  closeModal() {
+    this.modal.dismiss();
+  }
+
+  // Optional: Function to handle the data when publishing
+  onPublish() {
+    console.log('Event Published!');
+    this.modal.dismiss();
+  }
 }
